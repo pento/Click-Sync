79b0519dc1d2c915f853b56c2d0e1deb89a55f83
Gary Pendergast <gary@pento.net>
2018-04-15 23:13:13 +1000

    No need to artificially trim the history, now that garbage collection exists.

--- src/js/historyListener.js
+++ src/js/historyListener.js
@@ -39,10 +39,6 @@ function historyVisited( visit ) {
 			history = [];
 		}
 
-		if ( history.length > 10 ) {
-			history = history.slice( -10 );
-		}
-
 		history.push( visit.url );
 
 		var data = {};
