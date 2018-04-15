f06acca357d80042c4435a4360e441b15ff99211
Gary Pendergast <gary@pento.net>
2018-04-15 23:14:10 +1000

    Add Garbage Collection (#12)
    Fixes #6.

--- src/js/storageListener.js
+++ src/js/storageListener.js
@@ -3,16 +3,24 @@ function storageUpdated( changes, areaName ) {
 		return;
 	}
 
-	var localKey = 'history-' + id;
+	var localHistoryKey = 'history-' + id;
+	var localDeviceKey = 'device-' + id;
+
+	var syncedUrls = [];
 
 	for ( var key in changes ) {
-		if ( localKey === key ) {
+		if ( localHistoryKey === key ) {
+			continue;
+		}
+
+		if ( localDeviceKey === key ) {
 			continue;
 		}
 
 		if ( key.startsWith( 'history-' ) ) {
 			var lastSynced;
 			var syncingId = key.replace( 'history-', '' );
+
 			changes[ key ].newValue.forEach( function ( url )  {
 				if ( ! url ) {
 					return;
@@ -29,25 +37,69 @@ function storageUpdated( changes, areaName ) {
 				lastSynced = url;
 			} );
 
-			chrome.storage.sync.get( 'devices', function( value ) {
-				var devices = value.devices;
-				if ( typeof devices !== 'object' ) {
-					devices = {};
+			chrome.storage.sync.get( localDeviceKey, function( value ) {
+				var device = value[ localDeviceKey ];
+				if ( typeof device !== 'object' ) {
+					device = {};
 				}
 
-				if ( ! devices[ id ] ) {
-					devices[ id ] = {};
+				if ( typeof device.synced !== 'object' ) {
+					device.synced = {};
 				}
 
-				if ( ! devices[ id ].synced ) {
-					devices[ id ].synced = {};
-				}
+				device.synced[ syncingId ] = lastSynced;
 
-				devices[ id ].synced[ syncingId ] = lastSynced;
+				var data = {};
+				data[ localDeviceKey ] = device;
 
-				chrome.storage.sync.set( { devices: devices } );
+				chrome.storage.sync.set( data );
 			} );
 		}
+
+		if ( key.startsWith( 'device-' ) ) {
+			var deviceId = key.replace( 'device-', '' );
+
+			var syncedUrl = changes[ key ].newValue.synced[ id ];
+			if ( syncedUrl ) {
+				syncedUrls.push( syncedUrl );
+			}
+		}
+	}
+
+	if ( syncedUrls.length ) {
+		chrome.storage.sync.get( localHistoryKey, function( value ) {
+			var history = value[ localHistoryKey ];
+			if ( typeof history !== 'object' ) {
+				return;
+			}
+
+			var syncedUrlLocations = syncedUrls.map( function( url ) {
+				return history.indexOf( url );
+			} );
+
+			var oldestSyncedUrl = syncedUrlLocations.reduce( function( oldest, current ) {
+				if ( -1 === oldest ) {
+					return current;
+				}
+
+				if ( current > oldest ) {
+					return oldest;
+				}
+
+				return current;
+			}, -1 );
+
+			if ( oldestSyncedUrl < 0 ) {
+				return;
+			}
+
+			var unsyncedHistory = history.slice( oldestSyncedUrl );
+
+			var data = {};
+			data[ localHistoryKey ] = unsyncedHistory;
+
+			chrome.storage.sync.set( data );
+		} );
 	}
 }
 
