21ccd57000da656e8a0a6b3f144a5a1fb2f201af
Gary Pendergast <gary@pento.net>
2018-04-15 19:33:00 +1000

    Generate, store, and load a unique ID for the browser.
    Fixes #9.

--- src/js/historyListener.js
+++ src/js/historyListener.js
@@ -1,3 +1,33 @@
+var id;
+
+chrome.storage.local.get( 'id', function ( result ) {
+	if ( result.id ) {
+		id = result.id;
+	} else {
+		createLocalId();
+	}
+} );
+
+
+function createLocalId() {
+	function getRandomSymbol( symbol ) {
+        var array;
+
+        if ( symbol === 'y' ) {
+            array = [ '8', '9', 'a', 'b' ];
+            return array[ Math.floor( Math.random() * array.length ) ];
+        }
+
+        array = new Uint8Array( 1 );
+        window.crypto.getRandomValues( array );
+        return ( array[ 0 ] % 16 ).toString( 16 );
+    }
+
+	id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, getRandomSymbol );
+
+	chrome.storage.local.set( { id: id } );
+}
+
 function historyVisited( result ) {
 	if ( result.vistedCount > 1 ) {
 		return;
